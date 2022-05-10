7afce5b40ad418dadecae0e3fabba38e939916d6
Kobaysh <73707817+Kobaysh@users.noreply.github.com>
2022-05-10 16:58:43 +0900

    Feature/audio add (#23)
    * add:audio
    
    * update:Player SE

---
+++ Escape From Note/Assets/Scripts/GameScene/Player_Audio.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Player_Audio : MonoBehaviour {
+
+    // static field
+
+    // public member
+    public enum Player_SE {
+        PLAYER_SE_MOVE,
+        PLAYER_SE_MOVERAISED,
+        PLAYER_SE_JUMP,
+        PLAYER_SE_LANDING,
+        PLAYER_SE_DAMAGED,
+    };
+    
+    // serialized field
+
+    [SerializeField, Header("SE")]
+    AudioClip[] audioClips;
+    // private member
+
+    private AudioSource audioSource = null;
+
+
+
+    void Awake() {
+        
+    }
+
+    void Start () {
+        audioSource = GetComponent<AudioSource>();
+    }
+	
+
+    void Update () {
+	
+	}
+    
+    public void PlaySE(Player_SE audioName, bool isLoop = false)
+    {
+        audioSource.clip = audioClips[(int)audioName];
+        audioSource.loop = isLoop;
+        if(!audioSource.isPlaying) audioSource.Play();
+    }
+}
