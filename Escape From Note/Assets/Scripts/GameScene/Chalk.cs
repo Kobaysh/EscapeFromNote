9f5ceb00bc0565576809d0e55d0a940929bb42f8
Kobaysh <73707817+Kobaysh@users.noreply.github.com>
2022-05-24 17:46:37 +0900

    update:chalk color (#32)
    * update:chalk color
    
    * update:scene chalk type

--- Escape From Note/Assets/Scripts/GameScene/Chalk.cs
+++ Escape From Note/Assets/Scripts/GameScene/Chalk.cs
@@ -4,14 +4,45 @@ using UnityEngine;
 
 public class Chalk : MonoBehaviour
 {
+    public enum Chalk_Color
+    {
+        CHALK_WHITE,
+        CHALK_YELLOW,
+        CHALK_RED,
+        CHALK_BLUE,
+        CHALK_PINK,
+    }
+    [SerializeField]
+    private Chalk_Color chalkColor = Chalk_Color.CHALK_WHITE;
     [SerializeField]
     protected int healAmount = 1;
     [SerializeField]
     protected Player player;
+
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player").GetComponent<Player>();
+        switch (chalkColor)
+        {
+            case Chalk_Color.CHALK_WHITE:
+                this.GetComponent<MeshRenderer>().material.color = Color.white;
+                break;
+            case Chalk_Color.CHALK_YELLOW:
+                this.GetComponent<MeshRenderer>().material.color = Color.yellow;
+                break;
+            case Chalk_Color.CHALK_RED:
+                this.GetComponent<MeshRenderer>().material.color = Color.red;
+                break;
+            case Chalk_Color.CHALK_BLUE:
+                this.GetComponent<MeshRenderer>().material.color = Color.blue;
+                break;
+            case Chalk_Color.CHALK_PINK:
+                this.GetComponent<MeshRenderer>().material.color = new Color(1.0f, 0.6f, 0.9f, 1.0f);
+                break;
+            default: break;
+        }
+
     }
 
     // Update is called once per frame
@@ -25,7 +56,7 @@ public class Chalk : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             player.hp += healAmount;
-            player.GetComponent<MeshRenderer>().material = this.GetComponent<MeshRenderer>().material;
+            //player.ChangeColorみたいな関数で引数をChalk_Colorしてプレイヤーの色を変える
             Destroy(this.gameObject);
         }
     }
