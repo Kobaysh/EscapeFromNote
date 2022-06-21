86e67698306fe0508e5e2bb62f585c983d4e8acb
Kobaysh <73707817+Kobaysh@users.noreply.github.com>
2022-06-21 16:58:01 +0900

    add:kanji_bow

---
+++ Escape From Note/Assets/Scripts/GameScene/Kanji/Kanji_Bow/Kanji_Bow.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+ [CreateAssetMenu(menuName = "Assets/Kanji Scriptable/Kanji_Bow")]
+public class Kanji_Bow : Kanji_Abstract 
+{
+ 
+    // static field
+
+    // public member
+
+    // serialized field
+
+    // private member
+
+    public void Awake() 
+    {
+        
+    }
+
+    public void Start () 
+    {
+	
+	}
+	
+
+    public void Update () 
+    {
+	
+	}
+
+	// Action
+	public override void KanjiAction()
+	{
+
+	}
+
+	// unioncheck
+    public override bool KanjiUnionCheck()
+    {
+        return false;
+    }
+
+    // separation
+    public override void KanjiSeparation()
+    {
+
+    }
+}
